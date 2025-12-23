122709d8ba8ddbf341bc8a0d85380aef8d6dd28b
crazyminder12-ops <crazyminder12@gmail.com>
2025-12-23 12:33:07 +0500

    Refactor Customer class to add discount feature
    Updated Customer class to include discount functionality and modified constructors and methods accordingly.

--- src/Customer.java
+++ src/Customer.java
@@ -1,28 +1,78 @@
 public class Customer {
 
-    int id;
-    String name;
-    double money;
+    private int id;
+    private String name;
+    private double money;
+    private int discount;
 
-    public Customer(int id, String name, double money) {
+    public Customer(int id, String name, double money, int discount) {
         this.id = id;
         this.name = name;
         this.money = money;
+        this.discount = discount;
     }
 
-    double getMoney() {
+    public Customer() {
+        id = 0;
+        name = "none";
+        money = 0;
+        discount = 0;
+    }
+
+    // getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMoney() {
         return money;
     }
 
-    void setMoney(double money) {
+    public int getDiscount() {
+        return discount;
+    }
+
+    // setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        }
+    }
+
+    public void setMoney(double money) {
         if (money >= 0) {
             this.money = money;
         }
     }
 
-    void spend(double sum) {
+    public void setDiscount(int discount) {
+        if (discount >= 0 && discount <= 100) {
+            this.discount = discount;
+        }
+    }
+
+    // logic methods
+    public void spend(double sum) {
         if (sum <= money) {
             money -= sum;
         }
+    }
+
+    public double getPriceWithDiscount(double price) {
+        return price - (price * discount / 100.0);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{id=" + id + ", name='" + name + '\'' + ", money=" + money + ", discount=" + discount + "%}";
+                
     }
 }
