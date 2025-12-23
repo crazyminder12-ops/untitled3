e9c6cd6358cad29550fb16583d6ba4358fcc8c3a
crazyminder12-ops <crazyminder12@gmail.com>
2025-12-23 12:27:11 +0500

    Refactor Product class with private fields and constructors

--- src/Product.java
+++ src/Product.java
@@ -1,9 +1,9 @@
 public class Product {
 
-    int id;
-    String name;
-    double price;
-    int amount;
+    private int id;
+    private String name;
+    private double price;
+    private int amount;
 
     public Product(int id, String name, double price, int amount) {
         this.id = id;
@@ -12,29 +12,68 @@ public class Product {
         this.amount = amount;
     }
 
-    // getters
-    int getPrice() {
+    public Product() {
+        id = 0;
+        name = "none";
+        price = 0;
+        amount = 0;
+    }
+
+    
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
         return price;
     }
 
-    int getAmount() {
+    public int getAmount() {
         return amount;
     }
 
-    // setters
-    void setPrice(double price) {
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        }
+    }
+
+    public void setPrice(double price) {
         if (price >= 0) {
             this.price = price;
         }
     }
 
-    void setAmount(int amount) {
+    public void setAmount(int amount) {
         if (amount >= 0) {
             this.amount = amount;
         }
     }
 
-    void addAmount(int value) {
-        amount += value;
+
+    public void addAmount(int value) {
+        if (amount + value >= 0) {
+            amount += value;
+        }
+    }
+
+    public double getTotalValue() {
+        return price * amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{id=" + id + ", name='" + name + '\'' + ", price=" + price + ", amount=" + amount + '}';
+            
+              
     }
 }
